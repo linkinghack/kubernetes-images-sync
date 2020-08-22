d98e7d9ab4b9be1504900b2bb34a384c9a9d85e2
linkinghack <linkinghack@outlook.com>
2020-08-22 23:31:32 +0800

    Sync v1.19.0-rc.4

--- latest.sh
+++ latest.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-source versions/v1.17.11.sh
+source versions/v1.19.0-rc.4.sh
---
+++ versions/v1.19.0-rc.4.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+images=(
+    kube-apiserver:v1.19.0-rc.4
+    kube-controller-manager:v1.19.0-rc.4
+    kube-scheduler:v1.19.0-rc.4
+    kube-proxy:v1.19.0-rc.4
+    pause:3.2
+    etcd:3.4.3-0
+    coredns:1.6.7
+)
+
