0f4df5ffb4b7c92700a19f71b87003ea7d8c329a
linkinghack <linkinghack@outlook.com>
2020-08-22 23:04:10 +0800

    Update docker commands

--- main.sh
+++ main.sh
@@ -6,5 +6,6 @@ source latest.sh
 for imageName in ${images[@]} ; do
     docker pull k8s.gcr.io/$imageName
     docker tag k8s.gcr.io/$imageName  linkinghack/$imageName
-    docker push -u $DOCKERHUB_USER -p $DOCKERHUB_PASSWORD linkinghack/$imageName
+    docker login -u $DOCKERHUB_USER -p $DOCKERHUB_PASSWORD
+    docker push linkinghack/$imageName
 done
