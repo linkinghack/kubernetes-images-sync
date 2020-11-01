0babb9745f895fb65f5396aff4de59949b652278
linkinghack <linkinghack@outlook.com>
2020-11-01 18:23:15 +0800

    Update v1.19.0

---
+++ versions/v1.19.0.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+images=(
+    kube-apiserver:v1.19.0
+    kube-controller-manager:v1.19.0
+    kube-scheduler:v1.19.0
+    kube-proxy:v1.19.0
+    pause:3.2
+    etcd:3.4.9-1
+    coredns:1.7.0
+)
