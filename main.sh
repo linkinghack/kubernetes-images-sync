5df5501cfea7a9d2cffda604e15eaba771296b5e
Liulei <liuleiit01@chinamobile.com>
2021-12-16 11:43:36 +0800

    optimize docker login command

--- main.sh
+++ main.sh
@@ -3,11 +3,12 @@
 # Provide the 'images' array containing image names
 source latest.sh
 
+docker login -u $DOCKERHUB_USER -p $DOCKERHUB_PASSWORD
+
 # kubernetes base images
 for imageName in ${images[@]} ; do
     docker pull k8s.gcr.io/$imageName
     docker tag k8s.gcr.io/$imageName  linkinghack/$imageName
-    docker login -u $DOCKERHUB_USER -p $DOCKERHUB_PASSWORD
     docker push linkinghack/$imageName
 done
 
