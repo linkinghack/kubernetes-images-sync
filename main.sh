1769e7a7d2b1f354daf3d94e5e8337792833faed
linkinghack <linkinghack@outlook.com>
2022-02-11 11:04:55 +0800

    update prometheus-config-reloader to v0.54.0

--- main.sh
+++ main.sh
@@ -4,11 +4,11 @@
 docker login -u $DOCKERHUB_USER -p $DOCKERHUB_PASSWORD
 
 ## sync k8s basic components images
-bash sync-k8s.sh
+# bash sync-k8s.sh
 
 
 ## sync prometheus images
-# bash kube-prometheus/sync-k8s-prometheus.sh
+bash kube-prometheus/sync-k8s-prometheus.sh
 
 # sync knative images
 # bash knative/sync-eventing.sh
